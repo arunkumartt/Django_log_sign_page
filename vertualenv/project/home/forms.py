cbe7d70bd9c186142c76fd04f710b2bcf49d7c81
arunkumartt <arunkumartt05@gmail.com>
2023-05-24 13:49:55 +0530

    Initial commit

---
+++ vertualenv/project/home/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email','password1',)
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password')
